88aef77ddf0bbad71ef273200b87988823b41a6a
Anup Kanoor Gopalakrishnan <agopalakrishnan@manh.com>
2019-07-14 00:43:02 -0400

    t1

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t anupkg2017/client:latest -t anupkg2017/client:$SHA  -f ./client/Dockerfile ./client
+docker build -t anupkg2017/server:latest -t anupkg2017/server:$SHA -f ./server/Dockerfile ./server
+docker build -t anupkg2017/worker:latest -t anupkg2017/worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
-
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push anupkg2017/client:latest
+docker push anupkg2017/server:latest
+docker push anupkg2017/worker:latest
+docker push anupkg2017/client:$SHA
+docker push anupkg2017/server:$SHA
+docker push anupkg2017/worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+
+kubectl set image deployments/server-deployment.yaml server=anupkg2017/multi-server:$SHA
+kubectl set image deployments/client-deployment.yaml client=anupkg2017/multi-client:$SHA
+kubectl set image deployments/worker-deployment.yaml worker=anupkg2017/multi-worker:$SHA
